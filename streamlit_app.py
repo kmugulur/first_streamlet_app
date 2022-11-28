279ac112c9feb6d6b67da7b074c97568fc9a7d26
Keshava Mugulur <119322830+kmugulur@users.noreply.github.com>
2022-11-28 19:52:16 +0530

    Update streamlit_app.py
    added one more text line 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Hard boiled free-range egg')
