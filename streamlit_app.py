fbd66a0cf22d6da2f8bf4b8a8ebcb16452c4af84
Keshava Mugulur <119322830+kmugulur@users.noreply.github.com>
2022-12-04 17:28:30 +0530

    Update streamlit_app.py
    Few more changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,7 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # Look & feel changes - JSON normalization of fruityvice_response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Look & feel changes - Displaying in tabular form for easy readability
