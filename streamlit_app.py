7a93defa8a08028bdd96f48f348217625afab43a
Keshava Mugulur <119322830+kmugulur@users.noreply.github.com>
2022-12-04 17:24:40 +0530

    Update streamlit_app.py
    Look & Feel changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# Look & feel changes - JSON normalization of fruityvice_response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Look & feel changes - Displaying in tabular form for easy readability
+streamlit.dataframe(fruityvice_normalized)
