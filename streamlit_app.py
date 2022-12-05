648e92b2226a32856f932d941ebb133a890a4af2
Keshava Mugulur <119322830+kmugulur@users.noreply.github.com>
2022-12-05 16:14:07 +0530

    Update streamlit_app.py
    dynamic fruits enter facility

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ if streamlit.button('Get Fruit Load List'):
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+        my_cur.execute("insert into FRUIT_LOAD_LIST values ('" + ???? + "')")
         return "Thanks for adding " + new_fruit
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add')
@@ -76,4 +76,4 @@ if streamlit.button('Add Fruit Load List'):
     stremlit.text(back_from_function)
   
 #streamlit.write('Thanks for adding ', add_my_fruit)
-#my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+#my_cur.execute("insert into FRUIT_LOAD_LIST values ('" + ???? +"')")
