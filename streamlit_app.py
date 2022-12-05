769032849934c6ecbc70a7b93d74fea3178fc100
Keshava Mugulur <119322830+kmugulur@users.noreply.github.com>
2022-12-05 13:59:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urlerror.lib import URLError
+from urllib.error.lib import URLError
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
